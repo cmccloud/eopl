bcc656491a6fe57f3e907ea49f8cbf77bc36159a
Christopher McCloud <mccloud.christopher@gmail.com>
2016-04-29 16:59:08 -0300

    adds exercise 1.4

---
+++ chapter-1.rkt
@@ -0,0 +1,12 @@
+;; Exercise 1.1 - Notes
+;; Exercise 1.2 - Notes
+;; Exercise 1.3 - Notes
+;; Exercise 1.4 - Show that (-7 . (3 . (14 . ()))) is in List-of-Int
+;; List-of-Int
+;; (Int . List-of-Int)
+;; (-7 . List-of-Int)
+;; (-7 . (Int . List-of-Int))
+;; (-7 . (3 . List-of-Int))
+;; (-7 . (3 . (Int . List-of-Int)))
+;; (-7 . (3 . (14 . List-of-Int))
+;; (-7 . (3 . (14 . ()))
