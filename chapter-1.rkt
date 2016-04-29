f485cade91a72d51cc4e8e02e09a626173761e6c
Christopher McCloud <mccloud.christopher@gmail.com>
2016-04-29 17:47:48 -0300

    adds 1.7 and 1.8

--- chapter-1.rkt
+++ chapter-1.rkt
@@ -10,3 +10,35 @@
 ;; (-7 . (3 . (Int . List-of-Int)))
 ;; (-7 . (3 . (14 . List-of-Int))
 ;; (-7 . (3 . (14 . ()))
+;; Exercise 1.5 - Notes
+;; Exercise 1.6 - Notes
+;; Exercise 1.7
+(define (nth-element lst n)
+  "nth-element: List x Int => SchemeVal
+usage: (nth-element lst n) = the n-th element of lst"
+  (if (> n (length lst))
+      (report-list-too-short lst n)
+      (if (zero? n)
+          (car lst)
+          (nth-element (cdr lst) (- n 1)))))
+
+(define (report-list-too-short lst n)
+  (error 'nth-element "~a does not contain ~a elements" lst n))
+
+;; Exercise 1.8
+(define (drop-until s los)
+  "drop-until: Sym x Listof(Sym) => Listof(Sym)
+usage: (drop-until s los) = Listof(Sym) formed by dropping
+all elements up to the first instance of s, inclusive."
+  (if (null? los)
+      '()
+      (if (eqv? (car los) s)
+          (cdr los)
+          (drop-until s (cdr los)))))
+
+
+
+
+
+
+
