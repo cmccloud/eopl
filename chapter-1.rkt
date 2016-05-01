e409dd0feeb5f4b194e5947ebf02906440093082
Christopher McCloud <mccloud.christopher@gmail.com>
2016-05-01 14:43:36 -0300

    add 1.9 and tests

--- chapter-1.rkt
+++ chapter-1.rkt
@@ -25,6 +25,7 @@ usage: (nth-element lst n) = the n-th element of lst"
 (define (report-list-too-short lst n)
   (error 'nth-element "~a does not contain ~a elements" lst n))
 
+
 ;; Exercise 1.8
 (define (drop-until s los)
   "drop-until: Sym x Listof(Sym) => Listof(Sym)
@@ -36,8 +37,69 @@ all elements up to the first instance of s, inclusive."
           (cdr los)
           (drop-until s (cdr los)))))
 
+(define (test-1.8)
+  (and (equal? (drop-until 's '(a b c d s e f g))
+               '(e f g))
+       (equal? (drop-until 's '())
+               null)
+       (equal? (drop-until 'a '(b c d e f))
+               null)))
 
 
+;; Exercise 1.9
+(define (my-remove s los)
+  "my-remove: Sym x Listof(Sym) => Listof(Sym)
+usage: (my-remove s los) = Listof(Sym) formed by removing
+all instances of s from los"
+  (if (null? los) '()
+      (if (eqv? (car los) s)
+          (my-remove s (cdr los))
+          (cons (car los) (my-remove s (cdr los))))))
+
+(define (test-1.9)
+  (and (equal? (my-remove 's '(a b c s d s e f s s g))
+               '(a b c d e f g))
+       (equal? (my-remove 's '(a b c d e f))
+               '(a b c d e f))
+       (equal? (my-remove 's '(s s s s s s s))
+               null)))
+
+(define (occurs-free? var exp)
+  "occurs-free?: Sym x LcExp => Bool
+usage: returns #t if the symbol var occurs free in exp,
+otherwise returns #f"
+  (cond ((symbol? exp) (eqv? var exp))
+        ((eqv? (car exp) 'lambda)
+         (and (not (eqv? var (car (cadr exp))))
+              (occurs-free? var (caddr exp))))
+        (else
+         (or (occurs-free? var (car exp))
+             (occurs-free? var (cadr exp))))))
+
+(define (subst new old slist)
+  "subst: Sym x Sym x S-list => S-list"
+  (cond ((null? slist) null)
+        ((symbol? (car slist))
+         (if (eqv? (car slist) old)
+             (cons new (subst new old (cdr slist)))
+             (cons (car slist) (subst new old (cdr slist)))))
+        (else
+         (cons (subst new old (car slist))
+               (subst new old (cdr slist))))))
+
+(define (tree-each fn tree)
+  "tree-each: Fn x Tree => Tree"
+  (cond ((null? tree) null)
+        ((pair? (car tree))
+         (cons (tree-each fn (car tree))
+               (tree-each fn (cdr tree))))
+        (else (cons (fn (car tree))
+                    (tree-each fn (cdr tree))))))
+
+(define (subst2 new old slist)
+  (tree-each
+   (lambda (elt) (if (eqv? elt old) new elt))
+   slist))
 
 
 
